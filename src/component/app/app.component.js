20eb5ff6ac859e4d11432b0b9298fe2a90e7f03a
Robert Alexandru <romaniarobertverde@yahoo.com>
2020-01-24 17:34:40 +0000

    install style components + create the circle board component

--- src/component/app/app.component.js
+++ src/component/app/app.component.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import CircleBoard from '../circle-board'
 
 const App = () => {
   return (
-    <h2>Soon the game will ROCK</h2>
+    <div>
+      <h2>Soon the game will ROCK</h2>
+      <CircleBoard />
+    </div>
   )
 }
 
---
+++ src/component/circle-board/circle-board.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CircleBoard = () => {
+  return (
+    <h1>Circle Board is on it's way. It is cooking :)</h1>
+  )
+}
+
+export default CircleBoard
