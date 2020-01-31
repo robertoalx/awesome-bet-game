c3c6094af7b51f10c657e29bc71066304646bbce
Robert Alexandru <romaniarobertverde@yahoo.com>
2020-01-31 17:41:58 +0000

    create a numberList with 38 objects and each have 3 properties. We generate all the objects with Array.from method.

--- src/component/circle-board/circle-board.component.jsx
+++ src/component/circle-board/circle-board.component.jsx
@@ -3,6 +3,30 @@ import { StyledCircleBoard } from './circle-board.style'
 import Slot from '../slot'
 
 const CircleBoard = () => {
+  const length = 18
+  const numberList = [
+    {
+      number: '0',
+      rotate: 0,
+      color: 'green'
+    },
+    ...Array.from({ length }, (_, key) => key + 1).map (n => ({
+      number: n + '',
+      rotate: ~~(360 / 38 * n * 100) / 100,
+      color: n % 2 ? 'black' : 'red',
+    })),
+    {
+      number: '00',
+      rotate: 180,
+      color: 'green'
+    },
+    ...Array.from({ length }, (_, key) => key + 19).map (n => ({
+      number: n + '',
+      rotate: ~~(360 / 38 * (n + 1) * 100) / 100,
+      color: n % 2 ? 'red' : 'black',
+    }))
+  ]
+  console.log(numberList)
   return (
     <StyledCircleBoard side={350}>
       <Slot />
