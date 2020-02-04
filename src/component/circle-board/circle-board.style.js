bc99f7cf4018c01514d7eb48f3e9e49c11b50191
Robert Alexandru <romaniarobertverde@yahoo.com>
2020-02-04 15:56:56 +0000

    add an after circle that is position to the center of the main Big fat circle :)

--- src/component/circle-board/circle-board.style.js
+++ src/component/circle-board/circle-board.style.js
@@ -5,4 +5,16 @@ export const StyledCircleBoard = styled.div`
   border-radius: 50%;
   width: ${({ side }) => `${side }px`};
   height: ${({ side }) => `${side }px`};
+  position: relative;
+
+  &::after{
+    content: '';
+    position: absolute;
+    width: 250px;
+    height: 250px;
+    top: 50px;
+    left: 50px;
+    background-color: #764a25;
+    border-radius: 50%;
+  }
 `
