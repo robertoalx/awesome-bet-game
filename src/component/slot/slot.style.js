7d03498f778464b0189a469b2619d7b5aeefa715
Robert Alexandru <romaniarobertverde@yahoo.com>
2020-02-04 16:16:03 +0000

    Move each number to the center of his container and play with borders

--- src/component/slot/slot.style.js
+++ src/component/slot/slot.style.js
@@ -2,26 +2,29 @@ import styled from 'styled-components'
 
 export const StyleSlot = styled.div`
   height: 175px;
+  width: 30px;
   position: absolute;
-  // background-color: red;
   display: inline-block;
-  margin-left: 167px;
+  margin-left: 160px;
   transform-origin: 50% 100%;
   transform: rotate(${({ rotate }) => rotate}deg);
   color: white;
-
+  text-align: center;
+  
+  
   &::before{
     content: '';
     position: absolute;
     top: 0;
+    left: 0;
     border-top: 175px solid ${({ color }) => color};
-    border-left: 10px solid transparent;
-    border-right: 10px solid transparent;
+    border-left: 15px solid transparent;
+    border-right: 15px solid transparent;
+    border-radius: 5px;
   }
 
   &::after{
     content: ${({ number }) => `'${number}'`};
     position: relative;
-    text-align: center;
   }
 `
